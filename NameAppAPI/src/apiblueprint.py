b661421392ab7b02e9fb015c376379c0f02993ea
Francesco de Lorenzo <francescodelorenzo44@gmail.com>
2021-01-16 12:00:51 +0200

    initial commit and basic functionality of nameapp api

---
+++ NameAppAPI/src/apiblueprint.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+from src.namedb import NameDB
+import json
+
+api_blueprint = Blueprint("api_endpoints", __name__)
+
+name_db = NameDB("../res/names.json")
+print(name_db.select_names_by_amount_dsc())
+print(name_db.select_names_by_name_asc())
+print(name_db.select_name("Janne"))
+print(name_db.select_name("afea"))
+print(name_db.select_name("liisa"))
+print(name_db.select_sum_of_amounts())
+
+
+@api_blueprint.route("/names", methods=["GET"])
+def get_names():
+    return json.dumps(name_db.select_names_by_name_asc()), 200
+
+
+@api_blueprint.route("/name/<string:name>", methods=["GET"])
+def get_name(name):
+    return json.dumps(name_db.select_name(name))
+
+
+@api_blueprint.route("/amount", methods=["GET"])
+def get_amount():
+    return json.dumps({"sum_of_amounts": name_db.select_sum_of_amounts()})
+
---
+++ NameAppAPI/src/namedb.py
@@ -0,0 +1,26 @@
+import json
+
+
+class NameDB:
+    def __init__(self, filename):
+        with open(filename) as json_file:
+            self.name_data = json.load(json_file)["names"]
+            print(self.name_data)
+
+    def select_names_by_amount_dsc(self):
+        return sorted(self.name_data, key=lambda k: k["amount"], reverse=True)
+
+    def select_names_by_name_asc(self):
+        return sorted(self.name_data, key=lambda k: k["name"])
+
+    def select_name(self, name):
+        for item in self.name_data:
+            if item["name"].lower() == name.lower():
+                return item
+        return None
+
+    def select_sum_of_amounts(self):
+        total = 0
+        for item in self.name_data:
+            total = total + item["amount"]
+        return total
